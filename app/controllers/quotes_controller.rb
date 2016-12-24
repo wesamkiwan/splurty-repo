74dd193aa21f50433d32edb7c8640044e3279af0
wesamkiwan <wesamyk84@hotmail.com>
2016-12-24 02:02:43 -0500

    adding quotes form ready

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,8 +1,16 @@
 class QuotesController < ApplicationController
   def index
     @quote = Quote.order("RANDOM()").first
-    def new
-      @quote = Quote.new
-    end
   end
+  def new
+      @quote = Quote.new
+  end
+  def creat
+    Quote.create(quote_params)
+    redirect_to root_path
+  end 
+  private
+  def
+    params.require(:quote).permit(:saying , :aouther) 
+  end 
 end
