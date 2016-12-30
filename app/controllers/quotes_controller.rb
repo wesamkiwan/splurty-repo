47db2722a075bfd40878c87df9bb62d37abf4101
wesamkiwan <wesamyk84@hotmail.com>
2016-12-30 15:19:06 -0500

    correcting ofthe add-quote pop up box

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -2,9 +2,8 @@ class QuotesController < ApplicationController
   def index
     @quote = Quote.order("RANDOM()").first
   end
-  def new
-      @quote = Quote.new
-  end
+  
+  
   def create
         @quote = Quote.create(quote_params)
     if @quote.invalid?
